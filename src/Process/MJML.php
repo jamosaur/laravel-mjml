7d88f89ac73349142d87b38b4f6a10545fbae3da
James Wallen-Jones <j.wallen.jones@googlemail.com>
2022-08-17 15:13:21 +0100

    Update MJML.php

--- src/Process/MJML.php
+++ src/Process/MJML.php
@@ -2,7 +2,7 @@
 
 namespace Asahasrabuddhe\LaravelMJML\Process;
 
-use Soundsasleep\Html2Text;
+use Soundasleep\Html2Text;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\HtmlString;
 use Illuminate\View\View;
